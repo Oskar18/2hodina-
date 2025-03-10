80621d1ea83e253c0cae365d93fa10921d460720
Oskar18 <775334585@seznam.cz>
2025-03-10 21:23:36 +0100

    alll

--- github task1.py
+++ github task1.py
@@ -1,4 +1,4 @@
-import os
+import os #as something
 
 def task1(file_name, threshold=6):
     f = open(f"data/{file_name}", "r", encoding="utf-8")
@@ -20,7 +20,7 @@ def task1(file_name, threshold=6):
     f.writelines(new_data)
     f.close()
 
-"""task1("data.txt")
+#task1("data.txt")
 """
 with open("data.txt", "r") as f:
     for row in f:
@@ -30,7 +30,7 @@ with open("data.txt", "r") as f:
 with open("data.txt", "r") as f:
     rows = f.readlines()
     print(rows)"""
-
+"""
 with open("data/data.txt", "rb") as f:
     rows = f.read()
     print(rows)
@@ -40,4 +40,29 @@ if os.path.exists("data/filtered-data.txt"):
 else:
     print("Súbor neexistuje")"""
 
-    def task2
+
+
+def task2(file_name):
+    with (open(f"data/{file_name}", "r") as f_old,
+          open(f"data/copy-{file_name}", "w") as f_new):
+        data = f_old.read()
+        f_new.write(data)
+
+#task2("data.txt")
+
+def task3(file_name):
+    f = open(f"data/{file_name}", "r", encoding="utf-8")
+    data = f.readlines()
+    f.close()
+
+    print(data)
+    data.reverse()
+    print(data)
+    new_data = [f"{line.strip()}\n" for line in data]
+
+    f = open(f"reverse-{file_name}", "w")
+    f.writelines(new_data)
+    f.close()
+
+
+task3("data.txt")
